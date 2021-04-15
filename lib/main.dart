3d7fa3cba8bb5d5f9da503cb6c520ec223eb01c1
MahmoudHelmy <mahmoud.7elmy2013@gmail.com>
2021-04-16 01:06:35 +0200

    StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -4,32 +4,57 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+
+    return MyAppState();
+    throw UnimplementedError();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+
+  var lastIndex = 0;
+
+  void AnswerQuestion() {
+
+    setState(() {
+      lastIndex += 1;
+    });
+
+    print("Answer chosen");
+  }
+
   @override
   Widget build(BuildContext context) {
-    var questions = <String>[];
+    var questions = ["What is your name ?",
+      "How old are you ?"
+    ];
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("My Fitst App"),
+          title: Text(questions[lastIndex]),
         ),
         body: Column(
           children: [
             Text('The question'),
             RaisedButton(
               child: Text("Answer 1"),
-              onPressed: null,
+              onPressed: AnswerQuestion,
             ),
 
             RaisedButton(
               child: Text("Answer 2"),
-              onPressed: null,
+              onPressed: AnswerQuestion,
             ),
 
             RaisedButton(
               child: Text("Answer 3"),
-              onPressed: null,
+              onPressed: AnswerQuestion,
             ),
           ],
         ),
